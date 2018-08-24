8624fd5816c31b73cbdc21200cc727135df99599
randommzw <86463343@qq.com>
2018-08-24 19:29:27 +0800

    提交1

---
+++ src/main/java/com/heima/Test.java
@@ -0,0 +1,12 @@
+package com.heima;
+
+/**
+ * @author Mzw
+ * @version 1.0
+ * @date 2018/8/24   18:55
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("hello world!");
+    }
+}
